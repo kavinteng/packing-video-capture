4e4fa99edfbe655414fd7eec6a3536e7424643c6
kavinteng <76694895+kavinteng@users.noreply.github.com>
2021-06-11 10:47:57 +0700

    Add files via upload

--- pack.py
+++ pack.py
@@ -7,7 +7,9 @@ import datetime
 import numpy as np
 import requests
 import imutils
+import base64
 
+# decode QR code
 def decode(image):
     num = 0
     type = 0
@@ -17,8 +19,9 @@ def decode(image):
         image, x, y, w, h = draw_box(obj, image)
         num = obj.data
         type = obj.type
-    return image, num, type, x, y, w, h
+    return num, type, x, y, w, h
 
+# bounding box QR code
 def draw_box(decoded, image):
     x = decoded.rect.left
     y = decoded.rect.top
@@ -27,6 +30,7 @@ def draw_box(decoded, image):
     image = cv2.rectangle(image, (x, y),(x+w, y+h),color=(0, 255, 0),thickness=5)
     return image, x, y, w, h
 
+# edit video
 def cutvdo(mydata):
     os.chdir(vdo)
     data = cv2.VideoCapture('{}bc.mp4'.format(mydata))
@@ -40,18 +44,23 @@ def cutvdo(mydata):
         start = 0
     ffmpeg_extract_subclip('{}bc.mp4'.format(mydata), start, end, targetname='{}.mp4'.format(mydata))
 
+# post by requests to url
 def post_requests(mydata,url):
     os.chdir(vdo)
-    with open("{}.mp4".format(mydata), "rb") as a_file:
-        file_dict = {"file": a_file}
-        response = requests.post(url, files=file_dict)
+    file_name = "{}.mp4".format(mydata)
+    name, extension = os.path.splitext(file_name)
+    with open(file_name, "rb") as file:
+        text = base64.b64encode(file.read()).decode('utf-8')
+        data = {"data": text, "file_name": name, "file_type": extension}
+        response = requests.post(url, json=data)
 
         if response.ok:
             print("Upload completed successfully!")
-            # print(response.text)
+            # print(response.json())
         else:
             print("Something went wrong!")
 
+# load background image
 def getback():
     cap = cv2.VideoCapture(1)
     while True:
@@ -61,26 +70,29 @@ def getback():
     cap.release()
     cv2.destroyAllWindows()
 
-def checkback(gray):
+# check motion to auto video ending
+def checkback(gray,check,fh,fw):
     os.chdir(logo)
     img2 = cv2.imread("background.jpg")
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    img2 = cv2.resize(img2, (640, 480))
+    img2 = cv2.resize(img2, (fh, fw))
     frameDelta = cv2.absdiff(img2, gray)
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     for c in cnts:
-        if cv2.contourArea(c) < 10000:
+        if cv2.contourArea(c) < 30000:
             continue
-        check = 1
+        else:
+            print(cv2.contourArea(c))
+            check = 1
     return check
 
 def scanQR():
     cap = cv2.VideoCapture(1)
-    frame_w = int(cap.get(3))
-    frame_h = int(cap.get(4))
+    frame_w = int(cap.get(4))
+    frame_h = int(cap.get(3))
     record = 0
     array = []
 
@@ -89,10 +101,11 @@ def scanQR():
         _, frame = cap.read()
         if frame is None:
             continue
+        frame = cv2.resize(frame, (frame_h, frame_w))
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        frame_detect, data, type, x, y, w, h = decode(frame)
-        roi = frame_detect[y:y + h, x:x + w]
+        data, type, x, y, w, h = decode(frame)
+        roi = frame[y:y + h, x:x + w]
         if type == 'QRCODE' and record == 0:
             mydata = data.decode('utf-8')
             if len(array) < 3:
@@ -108,19 +121,19 @@ def scanQR():
         if record == 1:
             os.chdir(vdo)
             file = str(mydata)+"bc.mp4"
-            rec = cv2.VideoWriter(file, cv2.VideoWriter_fourcc(*'MP4V'),21, (frame_w, frame_h))
+            rec = cv2.VideoWriter(file, cv2.VideoWriter_fourcc(*'MP4V'),21, (frame_h, frame_w))
             record = 2
         if record == 2:
-            check = checkback(gray)
-            roi = frame_detect[-size-10:-10, -size-10:-10]
+            roi = frame[-size - 10:-10, -size - 10:-10]
             roi[np.where(mask)] = 0
             roi += img
-            cv2.putText(frame_detect, "ID: {}".format(str(mydata)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            cv2.putText(frame_detect, datetime.datetime.now().strftime("%D %T"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
-            rec.write(frame_detect)
-            cv2.imshow("Thresh", thresh)
-            cv2.imshow("Frame Delta", frameDelta)
-        cv2.imshow("test", frame_detect)
+            cv2.putText(frame, "ID: {}".format(str(mydata)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        (0, 0, 255), 2)
+            cv2.putText(frame, datetime.datetime.now().strftime("%D %T"), (10, frame.shape[0] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
+            rec.write(frame)
+            check = checkback(gray,check,frame_h,frame_w)
+        cv2.imshow("test", frame)
         k = cv2.waitKey(1)
         if check != 1 and record == 2:
             break
@@ -158,8 +171,8 @@ if __name__ == '__main__':
             try:
                 cutvdo(nameid)
                 os.remove('{}bc.mp4'.format(nameid))
-                url = "http://httpbin.org/post"
-                post_requests(nameid, url)
+                url = "https://globalapi.advice.co.th/api/upfile_json"
+                # post_requests(nameid, url)
             except:
                 pass
         elif check == "1":
