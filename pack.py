805e048ab521182ff8ecf7e83b05a5a788f5a287
kavinteng <76694895+kavinteng@users.noreply.github.com>
2021-06-20 20:51:13 +0700

    Add files via upload

--- pack.py
+++ pack.py
@@ -8,6 +8,7 @@ import numpy as np
 import requests
 import imutils
 import base64
+import HandTrackingModule as htm
 
 # decode QR code
 def decode(image):
@@ -45,13 +46,13 @@ def cutvdo(mydata):
     ffmpeg_extract_subclip('{}bc.mp4'.format(mydata), start, end, targetname='{}.mp4'.format(mydata))
 
 # post by requests to url
-def post_requests(mydata,url):
+def post_requests(nameid,orderid,url):
     os.chdir(vdo)
-    file_name = "{}.mp4".format(mydata)
+    file_name = "{}.mp4".format(orderid)
     name, extension = os.path.splitext(file_name)
     with open(file_name, "rb") as file:
         text = base64.b64encode(file.read()).decode('utf-8')
-        data = {"data": text, "file_name": name, "file_type": extension}
+        data = {"data": text, "Username": nameid, "Order ID": name, "file_type": extension}
         response = requests.post(url, json=data)
 
         if response.ok:
@@ -84,6 +85,7 @@ def checklogo(frame):
 
 # check motion to auto video ending
 def checkback(frame,check,fh,fw):
+    frame = cv2.flip(frame, 1)
     frame = cv2.resize(frame, (fh, fw))
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
@@ -106,18 +108,27 @@ def checkback(frame,check,fh,fw):
     cv2.imshow("thresh", thresh)
     return check
 
-def scanQR():
+def scanQR(record,font,st,nameid,orderid,login):
     cap = cv2.VideoCapture(1)
     frame_w = int(cap.get(4))
     frame_h = int(cap.get(3))
-    record = 0
-    array = []
+    orderid = "-"
     st = 0
+
+    detector = htm.handDetector(detectionCon=0.65, maxHands=1)
+    color = (0, 0, 255)
+
     while True:
         check = 0
         _, frame = cap.read()
         if frame is None:
             continue
+
+        frame = cv2.resize(frame, (640, 480))
+        frame = cv2.flip(frame, 1)
+        vdoframe = frame.copy()
+        frame = detector.findHands(frame)
+        lmList = detector.findPosition(frame, draw=False)
 
         # decode qr
         data, type, x, y, w, h = decode(frame)
@@ -126,19 +137,32 @@ def scanQR():
             if type == 'QRCODE':
                 st = 0
                 mydata = data.decode('utf-8')
-                # check that qr code in 3 sec is same
-                if len(array) < 3:
-                    array.append(mydata)
-                    time.sleep(1)
-                    continue
-                # save qr image
-                if len(set(array)) == 1:
-                    os.chdir(qrcode)
-                    word = str(mydata) + ".jpg"
-                    cv2.imwrite(word, roi)
-                    record = 1
-                array = []
-            else:
+                # # check that qr code in 3 sec is same
+                # if len(array) < 3:
+                #     array.append(mydata)
+                #     time.sleep(1)
+                #     continue
+                # # save qr image
+                # if len(set(array)) == 1:
+                #     os.chdir(qrcode)
+                #     word = str(mydata) + ".jpg"
+                #     cv2.imwrite(word, roi)
+                #     record = 1
+                # array = []
+                if mydata.isnumeric() == True:
+                    if login == False:
+                        nameid = "Please Login !!!"
+                        continue
+                    elif login == True:
+                        orderid = mydata
+                elif mydata.isnumeric() == False and login == False:
+                    if mydata == "":
+                        nameid = "username cannot empty"
+                        continue
+                    nameid = mydata
+                    login = True
+                    array = []
+            if login == False:
                 # in 30 sec, if not find any qr code it will break
                 if st == 0:
                     st = time.time()
@@ -146,34 +170,71 @@ def scanQR():
                     et = time.time()
                     if et-st > 30:
                         break
+        if login:
+            cv2.rectangle(frame, (300, 0), (380, 40), (0, 0, 255), cv2.FILLED)
+            cv2.putText(frame, "Log out", (310, 25), font, 0.5, (255, 255, 255), 2)
+            cv2.rectangle(frame, (400, 0), (480, 40), (255, 0, 255), cv2.FILLED)
+            cv2.putText(frame, "Record", (410, 25), font, 0.5, (255, 255, 255), 2)
+            cv2.rectangle(frame, (500, 0), (580, 40), (0, 0, 255), 2)
+            cv2.putText(frame, "STOP", (520, 25), font, 0.5, (0, 0, 255), 2)
+            cv2.putText(frame, f"Order ID : {str(orderid)}", (10, 50), font, 0.5, (0, 0, 255), 2)
+            if len(lmList) != 0:
+                # print(lmList)
+                x1, y1 = lmList[8][1:]
+                x2, y2 = lmList[12][1:]
+                cv2.circle(frame, (x1, y1), 15, color, cv2.FILLED)
+                fingers = detector.fingersUp()
+                if fingers[1] and fingers[2]:
+                    if y1 < 40:
+                        if 300 < x1 < 380 and record != 2 :
+                            color = (0, 255, 0)
+                            cv2.circle(frame, (x2, y2), 15, color, cv2.FILLED)
+                            login = False
+                            nameid = "-"
+                            orderid = "-"
+                        if 400 < x1 < 480 and orderid != "-" and record != 2:
+                            color = (0, 255, 0)
+                            cv2.circle(frame, (x2, y2), 15, color, cv2.FILLED)
+                            record = 1
+                        if 500 < x1 < 580 and record == 2:
+                            color = (0, 255, 0)
+                            cv2.circle(frame, (x2, y2), 15, color, cv2.FILLED)
+                            record = 3
+                else:
+                    color = (0, 0, 255)
         # create video file
         if record == 1:
             os.chdir(vdo)
-            file = str(mydata)+"bc.mp4"
+            file = str(orderid)+"bc.mp4"
             rec = cv2.VideoWriter(file, cv2.VideoWriter_fourcc(*'MP4V'),21, (frame_h, frame_w))
             record = 2
 
         # video recording
         if record == 2:
-            checklogo(frame)
-            cv2.putText(frame, "ID: {}".format(str(mydata)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+            checklogo(vdoframe)
+            cv2.putText(vdoframe, "Order ID: {}".format(str(orderid)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                         (0, 0, 255), 2)
-            cv2.putText(frame, datetime.datetime.now().strftime("%D %T"), (10, frame.shape[0] - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
-            rec.write(frame)
+            cv2.putText(vdoframe, datetime.datetime.now().strftime("%D %T"), (10, frame.shape[0] - 10),
+                        font, 0.4, (0, 0, 255), 2)
+            rec.write(vdoframe)
 
             # check motion to auto video ending. if user leave frame that is the end
             check = checkback(frame,check,frame_h,frame_w)
+        cv2.putText(frame, f"Log in as : {str(nameid)}", (10, 20), font, 0.5, (255, 0, 0), 2)
         cv2.imshow("test", frame)
+        cv2.imshow("vdo", vdoframe)
         k = cv2.waitKey(1)
         if check != 1 and record == 2:
+            break
+        if record == 3:
+            record = 0
             break
         if k == ord('q'):
             break
     cap.release()
     cv2.destroyAllWindows()
     try:
-        return mydata
+        return record,font,st,nameid,orderid,login
     except:
         pass
 
@@ -191,18 +252,27 @@ if __name__ == '__main__':
         pass
     # get background image
     getback()
+
+    record = 0
+    array = []
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    st = 0
+    nameid = "-"
+    orderid = "-"
+    login = False
+
     while True:
         # wait input to turn on camera
         wait_input = input("0 for cam, 1 for break: ")
         if wait_input == "0":
-            nameid = scanQR()
+            record,font,st,nameid,orderid,login = scanQR(record,font,st,nameid,orderid,login)
             try:
-                # create new and remove old
-                cutvdo(nameid)
-                os.remove('{}bc.mp4'.format(nameid))
-                # post to url
+                    # create new and remove old
+                cutvdo(orderid)
+                os.remove('{}bc.mp4'.format(orderid))
+                    # post to url
                 url = "https://globalapi.advice.co.th/api/upfile_json"
-                # post_requests(nameid, url)
+                # post_requests(nameid,orderid, url)
             except:
                 pass
         elif wait_input == "1":
