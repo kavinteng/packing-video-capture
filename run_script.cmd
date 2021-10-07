9b2868ddeed2581d4e09c5b8d40cf1342eb3103f
kavinteng <76694895+kavinteng@users.noreply.github.com>
2021-10-07 20:13:52 +0700

    Add files via upload

---
+++ run_script.cmd
@@ -0,0 +1,5 @@
+D:
+cd worklist\advice\test\packing-video-capture
+python D:\worklist\advice\test\packing-video-capture\check_network.py
+git pull
+python D:\worklist\advice\test\packing-video-capture\tk2.py
