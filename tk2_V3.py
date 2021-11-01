73490634f0afc8bc71e97e19b9370f1ff7946272
kavinteng <76694895+kavinteng@users.noreply.github.com>
2021-11-01 18:46:43 +0700

    Add files via upload

--- tk2_V3.py
+++ tk2_V3.py
@@ -202,6 +202,7 @@ class GUI(Tk):
         self.destroy()
 
 def repost():
+    global root2, i, form_edit
     try:
         connection = mariadb.connect(host="localhost", user="root", passwd="123456", database="advicev3")
     except mariadb.Error as e:
@@ -213,11 +214,9 @@ def repost():
         cursor.execute(TableSql)
     except:
         pass
-    global root2, i
+    form_edit = ''
     root2 = Tk()
     root2.title('repost')
-    cursor.execute("select * from backuppost limit 0,10")
-    lists = cursor.fetchall()
 
     e = Label(root2, width=11, text='ID', borderwidth=2, relief='ridge', anchor='w', bg='yellow')
     e.grid(row=0, column=0)
@@ -238,21 +237,23 @@ def repost():
     e = Label(root2, width=30, text='Error detail', borderwidth=2, relief='ridge', anchor='w', bg='yellow')
     e.grid(row=0, column=8)
 
-    i = 1
-    for list in lists:
-        for j in range(len(list)):
-            if j%8 == 0 and j != 0:
-                e = Label(root2, width=30, text=list[j],
-                            borderwidth=2, relief='ridge', anchor="w")
-                e.grid(row=i, column=j)
-            else:
-                e = Label(root2, width=11, text=list[j],
-                            borderwidth=2, relief='ridge', anchor="w")
-                e.grid(row=i, column=j)
-        # e = Button(root2, width=5, text='Edit', relief='ridge', bg='#32CD32',fg='white',
-        #               anchor="w", command=lambda k=list[0]: edit_data(k))
-        # e.grid(row=i, column=9)
-        i = i + 1
+    # i = 1
+    # for list in lists:
+    #     for j in range(len(list)):
+    #         if j%8 == 0 and j != 0:
+    #             e = Label(root2, width=30, text=list[j],
+    #                         borderwidth=2, relief='ridge', anchor="w")
+    #             e.grid(row=i, column=j)
+    #         else:
+    #             e = Label(root2, width=11, text=list[j],
+    #                         borderwidth=2, relief='ridge', anchor="w")
+    #             e.grid(row=i, column=j)
+    #
+    #     e = Button(root2, text='Check vdo', bg='#32CD32',fg='white'
+    #                   , command=lambda order=list[3],date=list[6],time=list[7]: check_vdo(order,date,time))
+    #     e.grid(row=i, column=9)
+    #     i = i + 1
+    i = list_realtime()
 
     connection.commit()
     connection.close()
@@ -263,16 +264,89 @@ def repost():
     edit_box.grid(row=i + 1, column=0, sticky='W', padx=5, pady=2)
     root2.mainloop()
 
+def list_realtime():
+    try:
+        connection = mariadb.connect(host="localhost", user="root", passwd="123456", database="advicev3")
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+    cursor = connection.cursor()
+    cursor.execute("select * from backuppost limit 0,10")
+    lists = cursor.fetchall()
+    i = 1
+    for list in lists:
+        for j in range(len(list)):
+            if j % 8 == 0 and j != 0:
+                e = Label(root2, width=30, text=list[j],
+                          borderwidth=2, relief='ridge', anchor="w")
+                e.grid(row=i, column=j)
+            else:
+                e = Label(root2, width=11, text=list[j],
+                          borderwidth=2, relief='ridge', anchor="w")
+                e.grid(row=i, column=j)
+
+        e = Button(root2, text='Check vdo', bg='#32CD32', fg='white'
+                   , command=lambda order=list[3], date=list[6], time=list[7]: check_vdo(order, date, time))
+        e.grid(row=i, column=9)
+        i = i + 1
+    return i
+
+def choices_id_list():
+    try:
+        connection = mariadb.connect(host="localhost", user="root", passwd="123456", database="advicev3")
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+    cursor = connection.cursor()
+    cursor.execute("select * from backuppost limit 0,10")
+    lists_nosize = cursor.fetchall()
+    choices_id = []
+    for list_nosize in lists_nosize:
+        if list_nosize[5] == '-':
+            choices_id.append(list_nosize[0])
+
+    connection.commit()
+    connection.close()
+    try:
+        return choices_id
+    except:
+        choices_id = ['']
+        return choices_id
+
+def check_vdo(order,date,time):
+    file_name = "D:/vdo_packing/{}/{}{}.mp4".format(date, order, time)
+    cap = cv2.VideoCapture(file_name)
+
+    while True:
+        _, frame = cap.read()
+        if frame is None:
+            break
+        cv2.rectangle(frame, (0,0), (640,85), (255,255,255), cv2.FILLED)
+        cv2.putText(frame, "Press q to escape", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                                (0, 0, 0), 2)
+        cv2.putText(frame, "Press p to play/pause", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                    (0, 0, 0), 2)
+        cv2.imshow('frame', frame)
+        k = cv2.waitKey(1)
+
+        if k == ord('q'):
+            break
+        elif k == ord('p'):
+            cv2.waitKey(0)
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 def editbox():
-    global entry1,entry2,text3
-    form_edit = ''
+    global entry1,entry2,text3,edit
     edit = Tk()
     edit.title('Edit box size')
     edit.geometry('300x50+1100+120')
     text1 = Label(edit,text='ID',borderwidth=2, relief='ridge', anchor="w", bg='yellow')
     text1.grid(row=0, column=0)
-    entry1 = Entry(edit, width= 11)
+    choices_id = choices_id_list()
+    entry1 = Combobox(edit, values=choices_id, width=6)
+    # entry1 = Entry(edit, width= 11)
     entry1.grid(row=0, column=1)
     text2 = Label(edit, text='Box size', borderwidth=2, relief='ridge', anchor="w", bg='yellow')
     text2.grid(row=0, column=2)
@@ -298,19 +372,40 @@ def addsize():
         sys.exit(1)
     global form_edit
     cursor = connection.cursor()
+    boxsize_array = ['A', 'B', 'C', 'D', 'E']
     ID = entry1.get()
     boxsize = entry2.get()
 
-    boxsize = '000'+ boxsize
-    form_edit = 'ID:{} SIZE:{}'.format(ID,boxsize)
-    if len(boxsize) == 4 and ID != '':
-        cursor.execute("update backuppost set size = ? where ID = ? ", (boxsize, ID))
-        text3.configure(text=form_edit)
+    detail = 'forget end (Added box size)'
+    boxsizes = '000'+ boxsize
+    ID = str(ID)
+
+    if ID == '':
+        form_edit = 'Error'
+        # text3.configure(text='All ID is done!!')
+    elif len(boxsizes) == 4:
+        if boxsize in boxsize_array:
+            form_edit = 'ID:{} SIZE:{}'.format(ID, boxsizes)
+            cursor.execute("update backuppost set size = ? where ID = ? ", (boxsizes, ID))
+            cursor.execute("update backuppost set detail = ? where ID = ? ", (detail, ID))
+            # text3.configure(text=form_edit)
+        else:
+            form_edit = 'Size not match'
     else:
-        text3.configure(text='Error')
+        form_edit = 'Error'
+        # text3.configure(text='ID not in database')
+
+    choices_id_list()
 
     connection.commit()
     connection.close()
+    list_realtime()
+    edit.destroy()
+    editbox()
+
+
+
+
 
 def post():
     try:
@@ -319,7 +414,7 @@ def post():
         print(f"Error connecting to MariaDB Platform: {e}")
         sys.exit(1)
     cursor = connection.cursor()
-    cursor.execute("select * from backuppost ")
+    cursor.execute("select * from backuppost limit 0,10")
     lists = cursor.fetchall()
     url = "https://globalapi.advice.co.th/api/upfile_json"
     for list in lists:
@@ -338,6 +433,7 @@ def post():
                     data = {"data": file}
                     text = {"Username": nameid, "Customer ID": customid, "Order ID": order, "Tel": tel, "Box size": size,
                             "file_type": extension, "token": encoded}
+
                     response = requests.post(url, files=data, data=text)
 
                     if response.ok:
